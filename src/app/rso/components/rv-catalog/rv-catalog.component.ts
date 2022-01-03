d6d01c7a8fc50b4df0d08bc16903f59220052807
jbyte23 <janbajt@gmail.com>
2022-01-03 09:20:08 +0100

    created component

---
+++ src/app/rso/components/rv-catalog/rv-catalog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rv-catalog',
+  templateUrl: './rv-catalog.component.html',
+  styleUrls: ['./rv-catalog.component.css']
+})
+export class RvCatalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
